4a05356ea669e176c3b3e06b393bc29907dd0cce
Yusuf Gürdoğan <yusufgurdogan@protonmail.com>
2026-06-16 13:00:23 +0300

    Update vouchers: remove 2 used, add 2 new

--- data/vouchers.js
+++ data/vouchers.js
@@ -8,9 +8,9 @@ const voucherData = {
         name: "VPS 8000 G12",
         discount: "FREE 1 MONTH",
         codes: [
-          "5802nc17812584120",
           "5802nc17813880120",
-          "5802nc17815608120"],
+          "5802nc17815608120",
+          "5802nc17816040120"],
       },
       {
         name: "VPS 4000 G12",
@@ -24,9 +24,9 @@ const voucherData = {
         name: "VPS 2000 G12",
         discount: "FREE 1 MONTH",
         codes: [
-          "5800nc17814744171",
           "5800nc17814744170",
-          "5800nc17815608170"],
+          "5800nc17815608170",
+          "5800nc17816040170"],
       },
       {
         name: "VPS 1000 G12",
